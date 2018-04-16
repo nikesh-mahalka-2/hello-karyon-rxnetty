ec1d407a03b3361920b4ab4da24425ad5f322513
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-04-16 18:15:34 +0530

    Update print.sh

--- print.sh
+++ print.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # test 2
-lsb_release -a
 echo Uploading artifact: http://dummy/repo/hello-karyon-rxnetty_1-1_all.deb
-curl -F file=@build/distributions/hello-karyon-rxnetty_1-1_all.deb http://35.197.61.70:8080/api/files/karyon
-curl -X POST http://35.197.61.70:8080/api/repos/my-karyon-rxnetty/file/karyon
-curl -X PUT -H 'Content-Type: application/json' --data '{"Distribution": "trusty", "ForceOverwrite": true, "Architectures":["amd64"], "Signing": {"Skip": true}, "SourceKind": "local", "Sources": [{"Name":"my-karyon-rxnetty"}]}' http://35.197.61.70:8080/api/publish/:./trusty
+#curl -F file=@build/distributions/hello-karyon-rxnetty_1-1_all.deb http://35.197.61.70:8080/api/files/karyon
+#curl -X POST http://35.197.61.70:8080/api/repos/my-karyon-rxnetty/file/karyon
+#curl -X PUT -H 'Content-Type: application/json' --data '{"Distribution": "trusty", "ForceOverwrite": true, "Architectures":["amd64"], "Signing": {"Skip": true}, "SourceKind": "local", "Sources": [{"Name":"my-karyon-rxnetty"}]}' http://35.197.61.70:8080/api/publish/:./trusty
+curl -X DELETE http://35.197.61.70:8080/api/publish//trusty
+curl -X DELETE http://35.197.61.70:8080/api/repos/hello-karyon-rxnetty
+curl -F file=@build/distributions/hello-karyon-rxnetty_1-1_all.deb http://35.197.61.70:8080/api/files/hello-karyon-rxnetty
+curl -X POST -H 'Content-Type: application/json' --data '{"Name": "hello-karyon-rxnetty"}' http://35.197.61.70:8080/api/repos
+curl -X POST http://35.197.61.70:8080/api/repos/hello-karyon-rxnetty/file/hello-karyon-rxnetty
+curl -X POST -H 'Content-Type: application/json' --data '{"Distribution": "trusty", "Architectures":["amd64"], "Signing": {"Skip": true}, "SourceKind": "local", "Sources": [{"Name":"hello-karyon-rxnetty"}]}' http://35.197.61.70:8080/api/publish
