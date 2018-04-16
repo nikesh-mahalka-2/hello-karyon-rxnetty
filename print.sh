8a320c68ab66064b772eda1ad5eec35b6cbdd657
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-04-16 16:50:06 +0530

    Update print.sh

--- print.sh
+++ print.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # test
 echo Uploading artifact: http://dummy/repo/hello-karyon-rxnetty_1-1_all.deb
+pwd
 curl -F file=@build/distributions/*.deb http://35.197.61.70:8080/api/files/hello-karyon-rxnetty
 curl -X POST http://35.197.61.70:8080/api/repos/hello-karyon-rxnetty/file/hello-karyon-rxnetty
 curl -X PUT -H 'Content-Type: application/json' --data '{"Distribution": "trusty", "ForceOverwrite": true, "Architectures":["amd64"], "Signing": {"Skip": true}, "SourceKind": "local", "Sources": [{"Name":"hello-karyon-rxnetty"}]}' http://35.197.61.70:8080/api/publish/:./trusty
